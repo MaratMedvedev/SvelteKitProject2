74d7c0cc37d08a9fdaf9e24df52048f04c35b5c8
MaratMedvedev <freiden2004@gmail.com>
2022-07-11 20:36:05 +0300

    first commit

---
+++ src/routes/contact.svelte
@@ -0,0 +1,45 @@
+<main>
+    <p class="textInContact">You can contact me using</p>
+    <div class="Logos">
+        <a href="https://t.me/DuckBlur">
+            <img
+                class="Logo"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Telegram_2019_Logo.svg/640px-Telegram_2019_Logo.svg.png"
+                alt=""
+            />
+        </a>
+        <a href="https://github.com/MaratMedvedev">
+            <img
+                class="Logo"
+                src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+                alt=""
+            />
+        </a>
+    </div>
+</main>
+
+<style>
+    p {
+        font-weight: 600;
+        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+            Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+    }
+    .textInContact {
+        text-align: center;
+        font-size: 60px;
+        margin-top: 1px;
+        color: rgb(23, 38, 99);
+    }
+    .Logos {
+        position: absolute;
+        top: 50%;
+        left: 45%;
+        text-align: center;
+    }
+
+    .Logo {
+        width: 60px;
+        height: 60px;
+        margin: 6px;
+    }
+</style>
---
+++ svelte.config.js
@@ -0,0 +1,23 @@
+
+import adapter from "@sveltejs/adapter-static";
+import preprocess from "svelte-preprocess";
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  preprocess: preprocess(),
+
+  kit: {
+    adapter: adapter(),
+    serviceWorker: {
+      register: false,
+    },
+    paths: {
+      // base: dev ? '' : '/bulatok.github.io',
+    },
+    prerender: {
+      default: true,
+    },
+  },
+};
+
+export default config;
