26b580769524229a2eece0647459f0378e6a3501
MaratMedvedev <freiden2004@gmail.com>
2022-07-11 20:54:17 +0300

    commit

--- svelte.config.js
+++ svelte.config.js
@@ -1,23 +1,22 @@
-
-import adapter from "@sveltejs/adapter-static";
-import preprocess from "svelte-preprocess";
+import adapter from '@sveltejs/adapter-static';
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: preprocess(),
+	preprocess: preprocess(),
 
-  kit: {
-    adapter: adapter(),
-    serviceWorker: {
-      register: false,
-    },
-    paths: {
-      // base: dev ? '' : '/bulatok.github.io',
-    },
-    prerender: {
-      default: true,
-    },
-  },
+	kit: {
+		adapter: adapter(),
+		serviceWorker: {
+			register: false
+		},
+		paths: {
+			// base: dev ? '' : '/MaratMedvedev.github.io',
+		},
+		prerender: {
+			default: true
+		}
+	}
 };
 
 export default config;
