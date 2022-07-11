bb28090188142750bfdd824f58cf3a2d30fc3fec
MaratMedvedev <freiden2004@gmail.com>
2022-07-11 21:11:22 +0300

    commit

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,14 @@
 // eslint-disable-next-line no-undef
 module.exports = {
 	env: {
-	  browser: true,
-	  es2021: true,
+		browser: true,
+		es2021: true
 	},
-	extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
-	parser: "@typescript-eslint/parser",
+	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+	parser: '@typescript-eslint/parser',
 	parserOptions: {
-	  ecmaVersion: "latest",
+		ecmaVersion: 'latest'
 	},
-	plugins: ["@typescript-eslint"],
-	rules: {},
-  };
+	plugins: ['@typescript-eslint'],
+	rules: {}
+};
